f856a2fa348821aef74384c3cbf6da242af8eb68
AnasArafeh <68680829+AnasArafeh@users.noreply.github.com>
2023-05-23 22:28:25 +0300

    Update ApplicationRoutes.tsx

--- src/Store/ApplicationRoutes.tsx
+++ src/Store/ApplicationRoutes.tsx
@@ -4,29 +4,33 @@ import NotFoundRoute from "../Components/NotFound/NotFound";
 
 const EntityLocationComponents = React.lazy(() => import('../Screens/EntityLocation/Index'));
 
-export const ApplicationRoutes = {
-    Entities: {
-        name: "Entities",
-        route: "/entity-location/",
-        roles: [],
-        component: <EntityLocationComponents component="editEntityLocation" />
-    },
-    NewEntity: {
-        name: "NewEntity",
-        route: "/entity/new-entity-location",
-        roles: [],
-        component: <EntityLocationComponents component="createEntityLocation" />
-    },
-    EditEntity: {
-        name: "EditEntity",
-        route: "/entity/edit-entity-location",
-        roles: [],
-        component: <EntityLocationComponents component="editLocation" />
-    },
-    NotFound: {
-        name: "NotFound",
-        route: "/404",
-        roles: [],
-        component: <NotFoundRoute />
+export const ApplicationRoutes = () => {
+
+
+    return {
+        Entities: {
+            name: "Entities",
+            route: "/entity-location/",
+            roles: [],
+            component: <EntityLocationComponents component="editEntityLocation" />
+        },
+        NewEntity: {
+            name: "NewEntity",
+            route: "/entity/new-entity-location",
+            roles: [],
+            component: <EntityLocationComponents component="createEntityLocation" />
+        },
+        EditEntity: {
+            name: "EditEntity",
+            route: "/entity/edit-entity-location",
+            roles: [],
+            component: <EntityLocationComponents component="editLocation" />
+        },
+        NotFound: {
+            name: "NotFound",
+            route: "/404",
+            roles: [],
+            component: <NotFoundRoute />
+        }
     }
 }
